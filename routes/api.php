1fdc4ab51eb6dd10aa475b821e3e5d44a7107344
ekanata14 <ekanata1411@gmail.com>
2025-07-07 00:18:53 +0800

    feat: api installment

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LandingPageController;
 
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong']);
+})->name('api.ping');
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/checkout/callback', [LandingPageController::class, 'midtransCallback'])->name('checkout.callback');
+Route::post('/checkout/callback', [LandingPageController::class, 'midtransCallback'])->name('api.checkout.callback');
